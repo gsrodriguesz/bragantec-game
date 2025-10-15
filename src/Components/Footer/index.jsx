3073af8e975bf03fd807d3fde01664e104e1e133
Gustavo Rodrigues <trashypg@gmail.com>
2025-10-14 21:47:13 -0300

    :lipstick: feat: footer style improvements

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -5,7 +5,7 @@ export function Footer() {
         <div className={styles.footer}>
             <div className={styles.pa}>
                 <p className={styles.pa}>Made with 💓 by <a href="https://github.com/gsrodriguesz">Gustavo Luiz</a> and <a href="https://github.com/Guugh">Gustavo Bettoi</a></p>
-                <p className={styles.pa}>3° Info - Class of 2025</p>
+                <p className={styles.pa}>3° Info - Class of 2025 💀</p>
             </div>
             <a href="/admin" className={styles.adminLink}>⚙️</a>
 
